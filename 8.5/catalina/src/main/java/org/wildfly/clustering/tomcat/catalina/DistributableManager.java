5257494753f759dc906b9640c69457094f1fcfc7
Paul Ferraro <paul.ferraro@redhat.com>
2017-01-05 11:13:29 -0500

    Rename

--- 8.5/catalina/src/main/java/org/wildfly/clustering/tomcat/catalina/DistributableManager.java
+++ 8.5/catalina/src/main/java/org/wildfly/clustering/tomcat/catalina/DistributableManager.java
@@ -47,7 +47,7 @@ import org.wildfly.clustering.web.session.SessionManager;
  * Adapts a WildFly distributable SessionManager to Tomcat's Manager interface.
  * @author Paul Ferraro
  */
-public class DistributableManager implements TomcatManager {
+public class DistributableManager implements CatalinaManager {
     private static final char ROUTE_DELIMITER = '.';
 
     private final SessionManager<LocalSessionContext, Batch> manager;
@@ -65,7 +65,7 @@ public class DistributableManager implements TomcatManager {
         this.marshallability = marshallability;
         this.context = context;
         this.route = ((Engine) context.getParent().getParent()).getJvmRoute();
-        this.invalidateAction = new TomcatSessionDestroyAction(context);
+        this.invalidateAction = new CatalinaSessionDestroyAction(context);
 
         this.manager.setDefaultMaxInactiveInterval(Duration.ofMinutes(context.getSessionTimeout()));
     }
